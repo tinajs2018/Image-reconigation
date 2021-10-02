3695d99145fce4f338ce6af20365f8f003e4545d
kristine karimi <64679787+tinajs2018@users.noreply.github.com>
2021-10-02 13:44:08 +0300

    Add files via upload

---
+++ image recongination model/untitled0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 20:01:23 2021
+
+@author: karimi
+"""
+
+import cv2
+import os
+import numpy as np
+#this module conttrain the function used to recognize the iimage
+#iven an image this function will return a rectangle  for face 
+def faceDetection(test.jpg):
